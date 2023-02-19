baae568cbf8292762e53764c0a65a13ab4d1d9f0
Md. Jafrin Hossin <kingjaaaf@gmail.com>
2023-02-19 18:30:01 +0600

    standartMiddleware

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import hpp from 'hpp' 
 import cookierSession from 'cookie-session'
+import compression from 'compression'
 import HTTP_STATUS from 'http-status-codes'
 import 'express-async-errors'
  
@@ -26,12 +27,35 @@ export class ChattyServer{
     private securityMiddleware(app: Application): void {
         app.use(
             cookierSession({
-                
+                name: 'session',
+                keys: ['key1', 'key2', 'key3'],
+                maxAge: 24 * 7 * 3600000,
+                secure: false 
+            })
+        )
+        app.use(hpp())
+        app.use(helmet())
+        app.use(
+            cors({
+                origin: '*',
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
             })
         )
     }
 
-    private standardMiddleware(app: Application): void {} 
+    private standardMiddleware(app: Application): void {
+        app.use(compression())
+        app.use(json({
+            limit: '50mb'
+        }))
+        app.use(urlencoded({
+            extended: true,
+            limit: '50mb'
+        }))
+
+    } 
 
     private routeMiddleware(app: Application): void {}
 
